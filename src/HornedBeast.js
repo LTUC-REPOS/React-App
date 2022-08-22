a183eb8b2dfb068a67c32bda3c93131b439d9afe
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2022-08-22 18:54:26 +0300

    fixing typos

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -5,7 +5,7 @@ class HornedBeast extends react.Component         {
     render(){
         return(
             <div>
-                <h2>The Blue Whale</h2>
+                <h2>{this.props.title}</h2>
                 <img src={this.props.link} alt={this.props.alt} title={this.props.title}/>                
                 <p>{this.props.desc}</p>
             </div>
