8982f2522792d5e1f51150c8cf11a33e4875d9f9
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2022-08-25 00:32:50 +0300

    Lab2 work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css'; 
-import React, { Component }  from 'react';
-import CardsContainer from './Components/CardsContainer';
+import "./App.css";
+import React, { Component } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
-class App extends Component  {
-  render(){
-  return (
-    <div className="App">
-      <CardsContainer />
-    </div>  
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
-}
-
 
 export default App;
